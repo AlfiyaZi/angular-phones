a0f3593f6e296d9c5493a7bcfe92094887f106a5
Alya <alfiya79@gmail.com>
2014-03-02 14:49:17 +0400

    step-2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,6 +2,16 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
-
+describe('PhoneCat controllers', function() {
+  beforeEach(module('phonecatApp'));
+ 
+  describe('PhoneListCtrl', function(){
+ 
+    it('should create "phones" model with 3 phones', inject(function($controller) {
+      var scope = {},
+          ctrl = $controller('PhoneListCtrl', { $scope: scope });
+ 
+      expect(scope.phones.length).toBe(3);
+    }));
+  });
 });
